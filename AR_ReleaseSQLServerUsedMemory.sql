3087a96b751979056956e6376f5850bd029b0c00
Ahmad Raeiji <32589448+arayeji@users.noreply.github.com>
2023-11-02 18:57:31 +0330

    Update AR_ReleaseSQLServerUsedMemory.sql

--- AR_ReleaseSQLServerUsedMemory.sql
+++ AR_ReleaseSQLServerUsedMemory.sql
@@ -1,53 +1,62 @@
-
 SET ANSI_NULLS ON
-GO
 SET QUOTED_IDENTIFIER ON
-GO
+
 -- =============================================
 -- Author:		Ahmad Raeiji
 -- Create date: 2021-06-01
 -- Description:	Releasing SQL Server Engine Used Memory
 -- =============================================
-create PROCEDURE AR_DecreaseSQLServerUsedMemory
+
+-- Create the stored procedure
+CREATE PROCEDURE AR_DecreaseSQLServerUsedMemory
 	@DesiredMaxMemory bigint, -- Desired Max Memory (MB)
 	@DecreaseBy int = 1024 -- The memory amount (MB) that will decrease in each step.
 AS
 BEGIN
+	-- Declare variables to hold current memory values
 	declare @CurrentMaxMemory bigint
 	declare @CurrentUsedMemory bigint
 	
-	SELECT @CurrentUsedMemory=(total_physical_memory_kb-available_physical_memory_kb)/1024 FROM sys.dm_os_sys_memory -- Calculating the amount of memory used by the SQL Server engine.
+	-- Calculate the amount of memory used by the SQL Server engine
+	SELECT @CurrentUsedMemory = (total_physical_memory_kb - available_physical_memory_kb) / 1024 FROM sys.dm_os_sys_memory
 	
-	SELECT @CurrentMaxMemory=cast([value_in_use] as bigint) FROM sys.configurations WHERE [name] = 'max server memory (MB)' -- Getting current max server memory
-
-	declare @LoopDetection int = ((@CurrentMaxMemory-@DesiredMaxMemory)/@DecreaseBy)+2 -- Calculating the maximum number of steps for loop prevention.
-
-	if @DesiredMaxMemory>@CurrentUsedMemory
+	-- Get the current max server memory value
+	SELECT @CurrentMaxMemory = cast([value_in_use] as bigint) FROM sys.configurations WHERE [name] = 'max server memory (MB)'
+	
+	-- Calculate the maximum number of steps for loop prevention
+	declare @LoopDetection int = ((@CurrentMaxMemory - @DesiredMaxMemory) / @DecreaseBy) + 2
+	
+	-- Check if the desired max memory is already lower than the current used memory
+	if @DesiredMaxMemory > @CurrentUsedMemory
 		begin
-			RAISERROR ('The current used memory is already lower than the desired maximum memory.',16,1)
+			RAISEERROR ('The current used memory is already lower than the desired maximum memory.', 16, 1)
 			RETURN
 		end
 	else
-	if @DesiredMaxMemory<@CurrentMaxMemory
+	-- Check if the desired max memory is already lower than the current max memory
+	if @DesiredMaxMemory < @CurrentMaxMemory
 		begin
-			RAISERROR ('The current max server memory is already lower than the desired maximum memory.',16,1)
+			RAISEERROR ('The current max server memory is already lower than the desired maximum memory.', 16, 1)
 			RETURN
 		end
 	
+	-- Enable advanced options
 	EXEC sys.sp_configure N'show advanced options', N'1'  RECONFIGURE WITH OVERRIDE
 	
-	while @CurrentMaxMemory>@DesiredMaxMemory and @LoopDetection>0
+	-- Start the memory adjustment loop
+	while @CurrentMaxMemory > @DesiredMaxMemory and @LoopDetection > 0
 		begin
-		    if @CurrentMaxMemory>@CurrentUsedMemory
-				Set @CurrentMaxMemory=@CurrentUsedMemory
-			Set @CurrentMaxMemory=@CurrentMaxMemory-@DecreaseBy
+		    if @CurrentMaxMemory > @CurrentUsedMemory
+				Set @CurrentMaxMemory = @CurrentUsedMemory
+			Set @CurrentMaxMemory = @CurrentMaxMemory - @DecreaseBy
 			EXEC sys.sp_configure N'max server memory (MB)', @CurrentMaxMemory
 			RECONFIGURE WITH OVERRIDE
 			waitfor delay '00:00:05' -- You can make changes to reduce interruptions caused by the SQL Server engine.
-			SELECT @CurrentMaxMemory=cast([value_in_use] as bigint) FROM sys.configurations WHERE [name] = 'max server memory (MB)'
-			Set @LoopDetection=@LoopDetection-1
+			SELECT @CurrentMaxMemory = cast([value_in_use] as bigint) FROM sys.configurations WHERE [name] = 'max server memory (MB)'
+			Set @LoopDetection = @LoopDetection - 1
 		end
 
+	-- Disable advanced options
 	EXEC sys.sp_configure N'show advanced options', N'0'  RECONFIGURE WITH OVERRIDE
 END
 GO
